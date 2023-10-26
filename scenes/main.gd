09f58890a5d85897f4718867a2fde4d0f9990017
Nimajjj <benjamin.borello@aol.com>
2023-10-26 09:31:39 +0200

    add map, obstacles, player and update main scene

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+## File:	main.gd
+##
+## Author:	Benjamin Borello
+## Created: 26/10/2023
+##
+## Main scene's script
+## It's only goal is to show how to get object (player)
+## and add it to scene through code.
 
-# Called when the node enters the scene tree for the first time.
+var player_scene: PackedScene = preload("res://scenes/player/player.tscn")
+
+@onready var player_spawn: Node2D = $PlayerSpawn
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_spawn_player()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _spawn_player() -> void:
+	var player: Player = player_scene.instantiate()
+	player.position = player_spawn.position
+	add_child(player)
---
+++ scenes/player/player.gd
@@ -0,0 +1,51 @@
+class_name Player
+extends CharacterBody2D
+
+## File:	player/player.gd
+##
+## Author:	Benjamin Borello
+## Created: 26/10/2023
+##
+## This script is an functional exemple 
+## for basics player character movement.
+
+const MAX_SPEED: float = 650.0
+const ACCELERATION: float = 0.25
+const FRICTION: float = 0.15
+
+@onready var camera: Camera2D = $Camera2D
+@onready var timer: Timer = $Timer
+
+
+func _ready() -> void:
+	camera.enabled = true
+
+
+func _physics_process(delta: float) -> void:
+	_movement_handler(delta)
+
+
+# Handle PC movements and apply Walk aninimation
+# use facing direction, acceleration and friction
+# @see _get_direction()
+func _movement_handler(delta: float) -> void:
+	var direction: Vector2 = _get_direction()
+	
+	if (direction.length() > 0):
+		velocity = lerp(velocity, direction * MAX_SPEED, ACCELERATION)
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, FRICTION)
+	
+	move_and_slide()
+
+
+# Calcul PC facing direction
+# Returned direction is normalized
+# Direction is compute from action_strength of 4 base axises
+func _get_direction() -> Vector2:
+	var d := Vector2.ZERO
+	
+	d.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	d.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	return d.normalized()
