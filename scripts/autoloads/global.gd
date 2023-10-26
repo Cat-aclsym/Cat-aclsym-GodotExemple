8e4a9726ea286234e914333c3d16cd7ec5479161
Nimajjj <benjamin.borello@aol.com>
2023-10-26 09:37:10 +0200

    add script info to global.gd

--- scripts/autoloads/global.gd
+++ scripts/autoloads/global.gd
@@ -1,5 +1,14 @@
 extends Node
 
+## File:	autoloads/global.gd
+##
+## Author:	Benjamin Borello
+## Created: 26/10/2023
+##
+## Autoload script.
+## Constains glabal variables
+## <!> Be carefull before getting variable from it.
+## <!> Always checks var != null
 
 var main: Node2D = null
 var player: Player = null
